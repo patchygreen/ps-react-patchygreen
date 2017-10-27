e8cfe474a05db49199d170287fcc34f77c31a13d
Crean, Patrick <Patrick.Crean@hmhco.com>
2017-10-27 11:00:46 +0100

    added TextInput with BEM CSS EXample and CSSModules

---
+++ src/docs/examples/TextInputCSSModules/ExampleOptional.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TextInputCSSModules from "ps-react/TextInputCSSModules";
+
+/** Optional TextBox */
+export default class ExampleOptional extends React.Component {
+  render() {
+    return (
+      <TextInputCSSModules
+        htmlId='example-optional'
+        label='First Name'
+        name='firstname'
+        onChange={() => {}}
+      />
+    )
+  }
+}
